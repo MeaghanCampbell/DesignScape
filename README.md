f6a9ffa34bd1c9d87114ee9d124a4493674b9b42
Meaghan Campbell <emeaghancampbell@gmail.com>
2023-10-17 08:12:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# DesignScape
+
+Template for some cool designs
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Card from './views/Card.vue'
+</script>
+
+<template>
+    <section class="px-4 sm:px-8 py-14 sm:py-20 max-w-6xl mx-auto">
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <Card />
+          <Card />
+          <Card />
+      </div>
+    </section>
+</template>
